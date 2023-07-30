53d34bb902d28e2677588d86071569d740a662cd
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-30 14:31:51 +0300

    feat: implement login profile and home screen navigator into App.jsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {store, persistor} from 'store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Navigation} from 'navigation/navigation';
+import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
-  return <Text>APP</Text>;
-};
+function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
+    </Provider>
+  );
+}
 
 export default App;
