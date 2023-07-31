d78d60ab44af1fa7cb541a98a5b520a4177590a1
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-31 13:52:43 +0300

    feat: implement transactions block to Home via dummy data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,33 @@
 import {ContainerWithHeader} from 'components/ContainerWithHeader';
 import {Wallet} from 'components/Wallet';
 import {TextWithIcon} from 'components/TextWithIcon';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {Dimensions, StyleSheet, View, ActivityIndicator} from 'react-native';
 import {theme} from 'theme';
 import {ActionButtonsBlock} from 'components/ActionButtonsBlock';
+import {TransactionList} from 'components/TransactionList';
+import {Transaction} from 'components/Transaction';
+import {user} from '../../constants';
+import {ITransaction} from './home';
 
 const WIDTH = Dimensions.get('window').width;
+
+const renderTransaction = ({item}: {item: ITransaction}) => (
+  <Transaction transaction={item} />
+);
 
 export const HomeScreen = () => {
   return (
     <ContainerWithHeader>
-      <Wallet currency="GBP" balance={19600.75} />
+      <Wallet currency={user.balance.currency} balance={user.balance.amount} />
       <View style={styles.infoContainer}>
         <TextWithIcon
           leftText="Hesap Numarası"
-          rightText="0000000"
+          rightText={user.accountNumber}
           icon="content-copy"
         />
         <TextWithIcon
           leftText="Sort Kod"
-          rightText="04-14-21"
+          rightText={user.sortCode}
           icon="check-circle-outline"
         />
       </View>
@@ -28,6 +36,12 @@ export const HomeScreen = () => {
         rightText="Para Yükle"
         leftIcon="user"
         rightIcon="credit-card"
+      />
+      <TransactionList
+        containerStyle={{marginTop: theme.spacing.xl}}
+        data={user.transactions}
+        renderItem={renderTransaction}
+        ListEmptyComponent={<ActivityIndicator />}
       />
     </ContainerWithHeader>
   );
